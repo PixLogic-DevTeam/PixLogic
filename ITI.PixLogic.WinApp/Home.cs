321fd25a83de4dcbebfb74833c3748ebca3873a8
Loïc <loic.donne95@gmail.com>
2015-06-02 09:57:30 +0200

    Import user data from CSV [WIP]
    Add a select file window for selecting the user's CSV file.

--- ITI.PixLogic.WinApp/Home.cs
+++ ITI.PixLogic.WinApp/Home.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.Entity;
+using System.IO;
 
 namespace ITI.PixLogic.WinApp
 {
@@ -31,10 +32,46 @@ namespace ITI.PixLogic.WinApp
             this.Hide();
         }
 
-        private void CapitalizedBtn_Click( object sender, EventArgs e )
+        private void CapitalizedBtn_Click(object sender, EventArgs e)
         {
-            new Capitalized( ).Show( );
-            this.Hide( );
+            new Capitalized().Show();
+            this.Hide();
+        }
+
+        private void cSVToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "CSV files (*.csv) | *.csv | All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            //MessageBox.Show("Ok");
+//                            LOAD DATA INFILE 'c:/tmp/discounts.csv' 
+//INTO TABLE discounts 
+//FIELDS TERMINATED BY ',' 
+//ENCLOSED BY '"'
+//LINES TERMINATED BY '\n'
+//IGNORE 1 ROWS;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
         }
     }
 }
