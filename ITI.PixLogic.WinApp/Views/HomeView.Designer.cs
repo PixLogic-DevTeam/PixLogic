c033c84f8f98a949330d0f9b7fbdf14cd19ca58c
Loic <donne@intechinfo.fr>
2015-06-19 15:58:07 +0200

    Fix errors
    - Change the syntax, of table
    - Update model from new database

--- ITI.PixLogic.WinApp/Views/HomeView.Designer.cs
+++ ITI.PixLogic.WinApp/Views/HomeView.Designer.cs
@@ -28,9 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.MainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.importationsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.utilisateursToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportationsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.objectsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.packsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.réservationsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.utilisateursToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.facturesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aProposToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contactsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MainMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainMenuStrip
+            // 
+            this.MainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.importationsToolStripMenuItem,
+            this.exportationsToolStripMenuItem,
+            this.aProposToolStripMenuItem});
+            this.MainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.MainMenuStrip.Name = "MainMenuStrip";
+            this.MainMenuStrip.Size = new System.Drawing.Size(784, 24);
+            this.MainMenuStrip.TabIndex = 0;
+            this.MainMenuStrip.Text = "MainMenuStrip";
+            // 
+            // importationsToolStripMenuItem
+            // 
+            this.importationsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.utilisateursToolStripMenuItem});
+            this.importationsToolStripMenuItem.Name = "importationsToolStripMenuItem";
+            this.importationsToolStripMenuItem.Size = new System.Drawing.Size(87, 20);
+            this.importationsToolStripMenuItem.Text = "Importations";
+            // 
+            // utilisateursToolStripMenuItem
+            // 
+            this.utilisateursToolStripMenuItem.Name = "utilisateursToolStripMenuItem";
+            this.utilisateursToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.utilisateursToolStripMenuItem.Text = "Utilisateurs";
+            this.utilisateursToolStripMenuItem.Click += new System.EventHandler(this.MainMenuStrip_Imports_Users_Click);
+            // 
+            // exportationsToolStripMenuItem
+            // 
+            this.exportationsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.objectsToolStripMenuItem,
+            this.packsToolStripMenuItem,
+            this.réservationsToolStripMenuItem,
+            this.utilisateursToolStripMenuItem1,
+            this.facturesToolStripMenuItem});
+            this.exportationsToolStripMenuItem.Name = "exportationsToolStripMenuItem";
+            this.exportationsToolStripMenuItem.Size = new System.Drawing.Size(84, 20);
+            this.exportationsToolStripMenuItem.Text = "Exportations";
+            // 
+            // objectsToolStripMenuItem
+            // 
+            this.objectsToolStripMenuItem.Name = "objectsToolStripMenuItem";
+            this.objectsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.objectsToolStripMenuItem.Text = "Objets";
+            // 
+            // packsToolStripMenuItem
+            // 
+            this.packsToolStripMenuItem.Name = "packsToolStripMenuItem";
+            this.packsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.packsToolStripMenuItem.Text = "Packs";
+            // 
+            // réservationsToolStripMenuItem
+            // 
+            this.réservationsToolStripMenuItem.Name = "réservationsToolStripMenuItem";
+            this.réservationsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.réservationsToolStripMenuItem.Text = "Réservations";
+            // 
+            // utilisateursToolStripMenuItem1
+            // 
+            this.utilisateursToolStripMenuItem1.Name = "utilisateursToolStripMenuItem1";
+            this.utilisateursToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.utilisateursToolStripMenuItem1.Text = "Utilisateurs";
+            this.utilisateursToolStripMenuItem1.Click += new System.EventHandler(this.MainMenuStrip_Exports_Users_Click);
+            // 
+            // facturesToolStripMenuItem
+            // 
+            this.facturesToolStripMenuItem.Name = "facturesToolStripMenuItem";
+            this.facturesToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.facturesToolStripMenuItem.Text = "Factures";
+            // 
+            // aProposToolStripMenuItem
+            // 
+            this.aProposToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.contactsToolStripMenuItem});
+            this.aProposToolStripMenuItem.Name = "aProposToolStripMenuItem";
+            this.aProposToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.aProposToolStripMenuItem.Text = "A propos";
+            // 
+            // contactsToolStripMenuItem
+            // 
+            this.contactsToolStripMenuItem.Name = "contactsToolStripMenuItem";
+            this.contactsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.contactsToolStripMenuItem.Text = "Contacts";
+            // 
+            // HomeView
+            // 
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.MainMenuStrip);
+            this.Name = "HomeView";
+            this.Text = "PixLogic";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.MainMenuStrip.ResumeLayout(false);
+            this.MainMenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip MainMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem importationsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportationsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem objectsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem packsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aProposToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem contactsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem utilisateursToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem réservationsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem utilisateursToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem facturesToolStripMenuItem;
 	}
 }
--- ITI.PixLogic.WinApp/Views/HomeView.cs
+++ ITI.PixLogic.WinApp/Views/HomeView.cs
@@ -1,8 +1,8 @@
-﻿using iTextSharp.text;
-using iTextSharp;
+﻿using iTextSharp;
+using iTextSharp.text;
 using iTextSharp.text.pdf;
-using ITI.PixLogic.DAL;
 using LumenWorks.Framework.IO.Csv;
+using ITI.PixLogic.DAL.Contexts.Accounts;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -17,147 +17,139 @@ using System.Windows.Forms;
 
 namespace ITI.PixLogic.WinApp
 {
-    public partial class HomeView : Form
-    {
-        AccountsEntity _accountsEntity = new AccountsEntity();
-        public HomeView()
-        {
-            InitializeComponent();
-        }
-		
-        private void cSVToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            Stream myStream = null;
-            OpenFileDialog openFileDialog = new OpenFileDialog();
+	public partial class HomeView : Form
+	{
+		AccountsEntity _accountsEntity = new AccountsEntity();
 
-            openFileDialog.InitialDirectory = "C:\\";
-            openFileDialog.Filter = "CSV files (*.csv) | *.csv | All files (*.*)|*.*";
-            openFileDialog.FilterIndex = 1;
-            openFileDialog.RestoreDirectory = true;
+		public HomeView()
+		{
+			InitializeComponent();
+		}
 
-            if (openFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                if ((myStream = openFileDialog.OpenFile()) != null)
+        #region Imports
+        private void MainMenuStrip_Imports_Users_Click( object sender, EventArgs e )
+		{
+			Stream myStream = null;
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+
+			openFileDialog.InitialDirectory = "C:\\";
+			//openFileDialog.Filter = "CSV files (*.csv) | *.csv | All files (*.*)|*.*"; /* Error with CSV Files */
+			openFileDialog.FilterIndex = 1;
+			openFileDialog.RestoreDirectory = true;
+
+			if (openFileDialog.ShowDialog() == DialogResult.OK)
+			{
+				if ((myStream = openFileDialog.OpenFile()) != null)
+				{
+					using (myStream)
+					{
+						// open the file openFileDialog.FileName which is a CSV file with headers
+						using (CsvReader csv =
+							   new CsvReader(new StreamReader(openFileDialog.FileName), true))
+						{
+							int fieldCount = csv.FieldCount;
+
+							string[] headers = csv.GetFieldHeaders();
+							while (csv.ReadNextRecord())
+							{
+								for (int i = 0; i < fieldCount; i++) string.Format("{0} = {1};", headers[i], csv[i]);
+
+								Account user = new Account();
+								user.FirstName = csv[1];
+								user.LastName = csv[2];
+								user.Email = csv[3];
+								user.Password = csv[4];
+								//user.active = Convert.ToBoolean(csv[5]);
+								user.Wallet = Convert.ToInt32(csv[6]);
+								var test = csv[7];
+								AccountSubCategory asc = _accountsEntity.AccountSubCategories.FirstOrDefault(o => o.Name == test);
+								asc.Name = csv[7];
+								user.SubCategory = asc.Id;
+								user.AccountSubCategory = asc;
+
+								var test2 = csv[8];
+								AccountMainCategory amc = _accountsEntity.AccountMainCategories.FirstOrDefault(o => o.Name == test2);
+								amc.Name = csv[8];
+								user.AccountSubCategory.AccountMainCategory = amc;
+
+								_accountsEntity.Accounts.Add(user);
+								_accountsEntity.SaveChanges();
+							}
+
+							MessageBox.Show("L'import de donnée a réussi.");
+						}
+					}
+				}
+			}
+
+		}
+        #endregion
+
+        #region Exports
+        private void MainMenuStrip_Exports_Users_Click( object sender, EventArgs e )
+		{
+			Document doc = new Document(PageSize.A4, 2, 2, 2, 2);
+			Paragraph p = new Paragraph("Export de la base de donnée en PDF.");
+			p.Alignment = Element.ALIGN_CENTER;
+
+			try
+			{
+                using( PdfWriter.GetInstance( doc, new FileStream( DateTime.Now + "-toutes-les-réservations.PDF", FileMode.Create ) ) )
                 {
-                    using (myStream)
+                    PdfPTable headers = new PdfPTable( 8 );
+                    PdfPTable infos = new PdfPTable( 8 );
+                    List<Account> data = new List<Account>();
+
+                    headers.HorizontalAlignment = 1;
+                    headers.SpacingBefore = 20f;
+                    headers.SpacingAfter = 20f;
+                    infos.HorizontalAlignment = 1;
+                    infos.SpacingBefore = 20f;
+                    infos.SpacingAfter = 20f;
+
+                    headers.AddCell( "ID" );
+                    headers.AddCell( "Prénom" );
+                    headers.AddCell( "Nom" );
+                    headers.AddCell( "Email" );
+                    headers.AddCell( "Mot de passe" );
+                    headers.AddCell( "Etat" );
+                    headers.AddCell( "Porte-monnaie" );
+                    headers.AddCell( "Sous-catégorie" );
+
+                    data = _accountsEntity.Accounts.OrderBy( a => a.FirstName ).ThenBy( a => a.LastName ).ThenBy( a => a.Password ).ToList();
+
+                    foreach( var Account in data )
                     {
-                        /// Clear table before add the data of csv
-                        _accountsEntity.Database.ExecuteSqlCommand("DELETE FROM accounts");
-
-                        // open the file openFileDialog.FileName which is a CSV file with headers
-                        using (CsvReader csv =
-                               new CsvReader(new StreamReader(openFileDialog.FileName), true))
-                        {
-                            int fieldCount = csv.FieldCount;
-
-                            string[] headers = csv.GetFieldHeaders();
-                            while (csv.ReadNextRecord())
-                            {
-                                for (int i = 0; i < fieldCount; i++) string.Format("{0} = {1};", headers[i], csv[i]);
-
-                                accounts user = new accounts();
-                                user.first_name = csv[1];
-                                user.last_name = csv[2];
-                                user.email = csv[3];
-                                user.password = csv[4];
-                                //user.active = Convert.ToBoolean(csv[5]);
-                                user.wallet = Convert.ToInt32(csv[6]);
-                                var test = csv[7];
-                                accounts_sub_categories asc = _accountsEntity.accounts_sub_categories.FirstOrDefault(o => o.name == test);
-                                asc.name = csv[7];
-                                user.sub_category = asc.id;
-                                user.accounts_sub_categories = asc;
-
-                                var test2 = csv[8];
-                                accounts_main_categories amc = _accountsEntity.accounts_main_categories.FirstOrDefault(o => o.name == test2);
-                                amc.name = csv[8];
-                                user.accounts_sub_categories.accounts_main_categories = amc;
-
-                                _accountsEntity.accounts.Add(user);
-                                _accountsEntity.SaveChanges();
-                            }
-
-                            MessageBox.Show("Import réussi");
-                        }
+                        infos.AddCell( Account.Id.ToString() );
+                        infos.AddCell( Account.FirstName );
+                        infos.AddCell( Account.LastName );
+                        infos.AddCell( Account.Email );
+                        infos.AddCell( Account.Password );
+                        infos.AddCell( Account.Active.ToString() );
+                        infos.AddCell( Account.Wallet.ToString() + " points" );
+                        infos.AddCell( Account.SubCategory.ToString() );
+                        doc.Open();
+                        doc.AddAuthor( "PixLogic PDF Generator" );
+                        doc.Add( p );
+                        doc.Add( headers );
+                        doc.Add( infos );
                     }
                 }
-            }
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Une erreur s'est produite (plus d'informations : " + ex.Message + ").");
+			}
+			finally
+			{
+				doc.Close();
+			}
 
-        }
-
-        private void Home_Load(object sender, EventArgs e)
-        {
-        }
-
-
-        private void utilisateursformatPDFToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
-            Document doc = new Document(PageSize.A4, 2, 2, 2, 2);
-            Paragraph p = new Paragraph("Export Database data to PDF file in c#");
-            p.Alignment = Element.ALIGN_CENTER;
-
-            try
-            {
-                PdfWriter.GetInstance(doc, new FileStream("réservations.pdf", FileMode.Create));
-
-                PdfPTable pdftable = new PdfPTable(8);
-                pdftable.HorizontalAlignment = 1;
-                pdftable.SpacingBefore = 20f;
-                pdftable.SpacingAfter = 20f;
-
-                pdftable.AddCell("ID");
-                pdftable.AddCell("Prénom");
-                pdftable.AddCell("Nom");
-                pdftable.AddCell("Email");
-                pdftable.AddCell("Mot de passe");
-                pdftable.AddCell("Etat");
-                pdftable.AddCell("Porte-monnaie");
-                pdftable.AddCell("Sous-catégorie");
-                
+			MessageBox.Show("Le fichier PDF a été créé !");
+		}
+        #endregion
 
 
-                PdfPTable pdftable2 = new PdfPTable(8);
-                pdftable2.HorizontalAlignment = 1;
-                pdftable2.SpacingBefore = 20f;
-                pdftable2.SpacingAfter = 20f;
-
-                List<accounts> data = new List<accounts>();
-                using (_accountsEntity)
-                {
-                    data = _accountsEntity.accounts.OrderBy(a => a.first_name).ThenBy(a => a.last_name).ThenBy(a => a.password).ToList();
-                }
-
-                foreach (var Account in data)
-                {
-                    pdftable2.AddCell(Account.id.ToString());
-                    pdftable2.AddCell(Account.first_name);
-                    pdftable2.AddCell(Account.last_name);
-                    pdftable2.AddCell(Account.email);
-                    pdftable2.AddCell(Account.password);
-                    pdftable2.AddCell(Account.active.ToString());
-                    pdftable2.AddCell(Account.wallet.ToString() + " points");
-                    pdftable2.AddCell(Account.sub_category.ToString());
-
-                    doc.Open();
-                    doc.AddAuthor("Loïc DONNE");
-                    doc.Add(p);
-                    doc.Add(pdftable);
-                    doc.Add(pdftable2);
-
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error" + ex.Message);
-            }
-
-            finally
-            {
-                doc.Close();
-            }
-
-            MessageBox.Show("PDF créer.");
-        }
     }
+
 }
