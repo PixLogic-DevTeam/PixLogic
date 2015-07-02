a6cda1314f9e1626c9881f8041edf9110a1e6db2
Smecta <Smecta@users.noreply.github.com>
2015-07-02 13:10:35 +0200

    Added Controls Ribbon menu
     - Added a database section with Controls
     - Added an imports section with Controls
     - Added a contact, configuration and exit Controls

--- ITI.PixLogic.WinApp/Views/MainView.Designer.cs
+++ ITI.PixLogic.WinApp/Views/MainView.Designer.cs
@@ -29,68 +29,70 @@
 		private void InitializeComponent()
 		{
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainView));
-			this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
-			this.PixLogicMainPanel = new System.Windows.Forms.Panel();
-			this.PixLogicRibbon = new RibbonLib.Ribbon();
+			this._printPreviewDialog = new System.Windows.Forms.PrintPreviewDialog();
+			this._ribbon = new RibbonLib.Ribbon();
+			this._panel = new System.Windows.Forms.Panel();
 			this.SuspendLayout();
 			// 
-			// printPreviewDialog1
+			// _printPreviewDialog
 			// 
-			this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
-			this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
-			this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
-			this.printPreviewDialog1.Enabled = true;
-			this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
-			this.printPreviewDialog1.Name = "printPreviewDialog1";
-			this.printPreviewDialog1.Visible = false;
+			this._printPreviewDialog.AutoScrollMargin = new System.Drawing.Size(0, 0);
+			this._printPreviewDialog.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+			this._printPreviewDialog.ClientSize = new System.Drawing.Size(400, 300);
+			this._printPreviewDialog.Enabled = true;
+			this._printPreviewDialog.Icon = ((System.Drawing.Icon)(resources.GetObject("_printPreviewDialog.Icon")));
+			this._printPreviewDialog.Name = "printPreviewDialog1";
+			this._printPreviewDialog.Visible = false;
 			// 
-			// PixLogicMainPanel
+			// _ribbon
 			// 
-			this.PixLogicMainPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+			this._ribbon.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this._ribbon.BackColor = System.Drawing.Color.White;
+			this._ribbon.Dock = System.Windows.Forms.DockStyle.None;
+			this._ribbon.Location = new System.Drawing.Point(0, 0);
+			this._ribbon.Margin = new System.Windows.Forms.Padding(0);
+			this._ribbon.Minimized = false;
+			this._ribbon.Name = "_ribbon";
+			this._ribbon.ResourceName = "ITI.PixLogic.WinApp.RibbonMarkup.ribbon";
+			this._ribbon.ShortcutTableResourceName = null;
+			this._ribbon.Size = new System.Drawing.Size(784, 144);
+			this._ribbon.TabIndex = 4;
+			// 
+			// _panel
+			// 
+			this._panel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.PixLogicMainPanel.BackColor = System.Drawing.Color.White;
-			this.PixLogicMainPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.PixLogicMainPanel.Location = new System.Drawing.Point(5, 149);
-			this.PixLogicMainPanel.Name = "PixLogicMainPanel";
-			this.PixLogicMainPanel.Size = new System.Drawing.Size(774, 408);
-			this.PixLogicMainPanel.TabIndex = 1;
+			this._panel.BackColor = System.Drawing.Color.White;
+			this._panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this._panel.Location = new System.Drawing.Point(5, 149);
+			this._panel.Name = "_panel";
+			this._panel.Size = new System.Drawing.Size(774, 408);
+			this._panel.TabIndex = 5;
 			// 
-			// PixLogicRibbon
-			// 
-			this.PixLogicRibbon.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.PixLogicRibbon.Dock = System.Windows.Forms.DockStyle.None;
-			this.PixLogicRibbon.Location = new System.Drawing.Point(0, 0);
-			this.PixLogicRibbon.Minimized = false;
-			this.PixLogicRibbon.Name = "PixLogicRibbon";
-			this.PixLogicRibbon.ResourceName = "ITI.PixLogic.WinApp.RibbonMarkup.ribbon";
-			this.PixLogicRibbon.ShortcutTableResourceName = null;
-			this.PixLogicRibbon.Size = new System.Drawing.Size(784, 144);
-			this.PixLogicRibbon.TabIndex = 0;
-			// 
-			// MainFormView
+			// MainView
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
 			this.ClientSize = new System.Drawing.Size(784, 562);
-			this.Controls.Add(this.PixLogicMainPanel);
-			this.Controls.Add(this.PixLogicRibbon);
+			this.Controls.Add(this._ribbon);
+			this.Controls.Add(this._panel);
 			this.MinimumSize = new System.Drawing.Size(800, 600);
-			this.Name = "MainFormView";
+			this.Name = "MainView";
 			this.Text = "PixLogic";
 			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-			this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainFormView_Paint);
-			this.Resize += new System.EventHandler(this.MainFormView_Resize);
+			this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_OnPaint);
+			this.Resize += new System.EventHandler(this.MainForm_OnResize);
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
-		private System.Windows.Forms.Panel PixLogicMainPanel;
-		private RibbonLib.Ribbon PixLogicRibbon;
+		private System.Windows.Forms.PrintPreviewDialog _printPreviewDialog;
+		private RibbonLib.Ribbon _ribbon;
+		private System.Windows.Forms.Panel _panel;
 	}
 }
---
+++ Licensing/README.md
@@ -0,0 +1,21 @@
+#Licensing Informations#
+
+---
+
+>The *Ribbon for Windows Forms* (*RibbonLib*) library:
+>
+> **Microsoft Public License** (**MS-PL**)
+
+---
+
+>The *Farm Fresh Icons* by *Fatcow Web Hosting* used in the user interface:
+>
+> **Creative Commons Attribution 4.0** (**CC Attribution 4.0**)
+
+---
+
+>Every others parts of this software:
+>
+> **GNU General Public License** (**GPL**)      
+
+---                         
